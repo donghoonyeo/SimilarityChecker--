189af451aa6adf5025322d9af403a7126fbf7ea3
donghoonyeo <www1w1w11@gmail.com>
2024-05-31 09:57:10 +0900

    length test case 추가 및 refactoring

--- Project17/similarity.cpp
+++ Project17/similarity.cpp
@@ -1,19 +1,26 @@
 #include <string>
-
+#include <stdexcept>
 using namespace std;
 
 class Similar {
 public:
-	float lengthTest(string target1, string target2) {
+	int getLengthTestResult(string target1, string target2) {
 		return getScore(target1.size(), target2.size());
 	}
 
-	float getScore(int val1, int val2) {
+	int getScore(int val1, int val2) {
+		if (val1 == 0 && val2 == 0)
+			assertIllegalArgument();
 		int smallValue = (val1 > val2) ? val2 : val1;
 		int bigValue = smallValue == val1 ? val2 : val1;
 
 		if (bigValue > smallValue * 2) return 0;
 
 		return (((smallValue * 2) - bigValue) * 60) / smallValue;
+	}
+
+private:
+	void assertIllegalArgument() {
+		throw invalid_argument("Both string size must be more than 0");
 	}
 };
--- Sample-Test1/similarity_test.cpp
+++ Sample-Test1/similarity_test.cpp
@@ -4,22 +4,48 @@
 class SimilarityFixture : public testing::Test {
 public:
 	Similar checkDiff;
+
+	void assertIllegalArgument(string target1, string target2) {
+		try {
+			checkDiff.getLengthTestResult(target1, target2);
+			FAIL();
+		}
+		catch (exception e) {
+			// PASS
+		}
+	}
 };
 
 TEST_F(SimilarityFixture, StringLengthScore60) {
-	int actual = checkDiff.lengthTest("ABC", "BAC");
+	int actual = checkDiff.getLengthTestResult("ABC", "BAC");
 	int expected = 60;
 	EXPECT_EQ(actual, expected);
 }
 
 TEST_F(SimilarityFixture, StringLengthScore30) {
-	int actual = checkDiff.lengthTest("ABC", "BA");
+	int actual = checkDiff.getLengthTestResult("ABC", "BA");
 	int expected = 30;
 	EXPECT_EQ(actual, expected);
 }
 
+TEST_F(SimilarityFixture, StringLengthScore0) {
+	int actual = checkDiff.getLengthTestResult("ABCDEFGT", "BAC");
+	int expected = 0;
+	EXPECT_EQ(actual, expected);
+}
+
 TEST_F(SimilarityFixture, StringLengthScorefloat) {
-	int actual = checkDiff.lengthTest("ABCDEFGT", "BACQZPD");
+	int actual = checkDiff.getLengthTestResult("ABCDEFGT", "BACQZPD");
 	int expected = 360 / 7;
 	EXPECT_EQ(actual, expected);
+}
+
+TEST_F(SimilarityFixture, StringLengthZero1) {
+	int actual = checkDiff.getLengthTestResult("ABCDEFGT", "");
+	int expected = 0;
+	EXPECT_EQ(actual, expected);
+}
+
+TEST_F(SimilarityFixture, StringLengthZero2) {
+	assertIllegalArgument("", "");
 }
