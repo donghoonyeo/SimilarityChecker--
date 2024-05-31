f1848aea4f1eca8190a35564adf99e064cf64bde
donghoonyeo <www1w1w11@gmail.com>
2024-05-31 09:27:27 +0900

    [refactoring] test fixture 적용 및  점수 계산부 기능구현 및 리팩토링

--- Project17/similarity.cpp
+++ Project17/similarity.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 class Similar {
 public:
-	int lengthTest(string target1, string target2) {
-		return 60;
+	float lengthTest(string target1, string target2) {
+		return getScore(target1.size(), target2.size());
+	}
+
+	float getScore(int val1, int val2) {
+		int smallValue = (val1 > val2) ? val2 : val1;
+		int diff = (val1 > val2) ? (val1 - val2) : (val2 - val1);
+
+		if (diff / smallValue > 1) return 0;
+
+		return (1 - float(diff) / float(smallValue)) * 60;
+
 	}
 };
--- Sample-Test1/similarity_test.cpp
+++ Sample-Test1/similarity_test.cpp
@@ -1,9 +1,13 @@
 #include "pch.h"
 #include "../Project17/similarity.cpp"
 
-TEST(SimilarityChecker, StringLengthScore60) {
+class SimilarityFixture : public testing::Test {
+public:
 	Similar checkDiff;
-	int actual = checkDiff.lengthTest("ABC", "BAC");
-	int expected = 60;
+};
+
+TEST_F(SimilarityFixture, StringLengthScore60) {
+	float actual = checkDiff.lengthTest("ABC", "BAC");
+	float expected = 60;
 	EXPECT_EQ(actual, expected);
 }
