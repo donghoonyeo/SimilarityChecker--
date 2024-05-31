04d348ef3e5cff17c81ed4ef410eff78a087f424
donghoonyeo <www1w1w11@gmail.com>
2024-05-31 10:07:14 +0900

    Update similarity.cpp
    리뷰 의견 반영하여 수정하였습니다.

--- Project17/similarity.cpp
+++ Project17/similarity.cpp
@@ -8,19 +8,19 @@ public:
 		return getScore(target1.size(), target2.size());
 	}
 
-	int getScore(int val1, int val2) {
-		if (val1 == 0 && val2 == 0)
+private:
+	void assertIllegalArgument() {
+		throw invalid_argument("Both string size must be more than 0");
+	}
+
+	int getScore(int size1, int size2) {
+		if (size1 == 0 && size2 == 0)
 			assertIllegalArgument();
-		int smallValue = (val1 > val2) ? val2 : val1;
-		int bigValue = smallValue == val1 ? val2 : val1;
+		int smallValue = (size1 > size2) ? size2 : size1;
+		int bigValue = smallValue == size1 ? size2 : size1;
 
 		if (bigValue > smallValue * 2) return 0;
 
 		return (((smallValue * 2) - bigValue) * 60) / smallValue;
-	}
-
-private:
-	void assertIllegalArgument() {
-		throw invalid_argument("Both string size must be more than 0");
 	}
 };
